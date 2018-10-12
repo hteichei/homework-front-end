d564bdf864879d2aa516c32f983e2e39ffcb2a95
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-12 16:09:21 -0700

    Added Components folder and created GifList component

---
+++ src/Components/GifList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>I'm a List</h1>
+      </div>
+    );
+  }
+}
+
+export default GifList;
