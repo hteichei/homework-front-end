a16be7f024b159c03215289a235a6e4830ffb922
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-12 18:03:54 -0700

    Moved state up to App Component, pass state down to GifList as props

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import GifList from '../GifList';
 import Header from '../Header';
 
 class App extends Component {
+  state = { gifs: [] };
+
+  async componentDidMount() {
+    const response = await this.fetchTrendingGifs();
+    this.setState({
+      gifs: response
+    });
+  }
+
+  fetchTrendingGifs = async () => {
+    try {
+      const apiKey = `${process.env.REACT_APP_GIF_API_KEY}`;
+      const url = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
+      const response = await axios.get(url).then(res => {
+        return res.data.data;
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
-        <GifList />
+        <GifList gifList={this.state.gifs} />
       </div>
     );
   }
--- src/Components/GifList/index.js
+++ src/Components/GifList/index.js
@@ -1,33 +1,10 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import GifCard from '../GifCard';
 import './style.css';
 
 class GifList extends Component {
-  state = { gifs: [] };
-
-  async componentDidMount() {
-    const response = await this.fetchTrendingGifs();
-    this.setState({
-      gifs: response
-    });
-  }
-
-  fetchTrendingGifs = async () => {
-    try {
-      const apiKey = `${process.env.REACT_APP_GIF_API_KEY}`;
-      const url = `http://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`;
-      const response = await axios.get(url).then(res => {
-        return res.data.data;
-      });
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   render() {
-    const grid = this.state.gifs.map(gif => {
+    const grid = this.props.gifList.map(gif => {
       return (
         <GifCard
           key={gif.id}
