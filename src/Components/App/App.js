56c9d9ec6832959b8dfb2ef0cfe2a8307eeb0dc1
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-12 18:22:27 -0700

    Added function to fetch gifs by search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,12 +5,17 @@ import GifList from '../GifList';
 import Header from '../Header';
 
 class App extends Component {
-  state = { gifs: [] };
+  state = {
+    trendingGifs: [],
+    trending: true,
+    searchResults: [],
+    search: false
+  };
 
   async componentDidMount() {
     const response = await this.fetchTrendingGifs();
     this.setState({
-      gifs: response
+      trendingGifs: response
     });
   }
 
@@ -27,11 +32,43 @@ class App extends Component {
     }
   };
 
+  toggleSearchOn = () => {
+    this.setState({
+      ...state,
+      searchResults: [],
+      search: true
+    });
+  };
+
+  fetchGifsBySearch = async searchParams => {
+    try {
+      //clear previous search results in state before new request
+      //set this.state.search to true
+
+      this.toggleSearchOn();
+      const apiKey = `${process.env.REACT_APP_GIF_API_KEY}`;
+      let searchParams = 'funny cat';
+      const url = `http://api.giphy.com/v1/gifs/search?q=${searchParams}&api_key=${apiKey}`;
+      const response = await axios.get(url).then(res => {
+        return res.data.data;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
-        <GifList gifList={this.state.gifs} />
+        <GifList
+          gifList={
+            //if search is active, render search results, otherwise render trending gifs
+            this.state.search
+              ? this.state.searchResults
+              : this.state.trendingGifs
+          }
+        />
       </div>
     );
   }
