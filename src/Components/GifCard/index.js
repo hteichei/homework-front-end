aed313b0701baa2ec6af3a50fd8bb1897efbcdd1
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-13 17:52:28 -0700

    Added fade in transition to loading images

--- src/Components/GifCard/index.js
+++ src/Components/GifCard/index.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './style.css';
 import LazyLoad from 'react-lazy-load';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class GifCard extends Component {
+  state = { loading: true };
+
+  handleLoading = () => {
+    this.setState({ loading: false });
+  };
+
   render() {
     const { id, title, rating, username, src } = this.props;
     return (
       <div className="card">
+        {}
         <LazyLoad debounce={false} height={250} width={250} offset={0}>
-          <img src={src} alt="gif" className="img-thumnail" />
+          <img
+            src={src}
+            alt="gif"
+            className={this.state.loading ? 'loading' : 'loaded'}
+            onLoad={this.handleLoading}
+          />
         </LazyLoad>
       </div>
     );
